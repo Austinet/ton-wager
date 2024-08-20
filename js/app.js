8df6ce74270d577e957865a9ee8f2de477fb8f64
Austinet <austinetsplendour@gmail.com>
2024-08-20 08:49:55 +0100

    ft: adds Audio player and its assets. Changes the steps images

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // Changes texts and colors synchronously 
 const textArray = ['Ton Blockchain', 'Telegram']
 
-let changedTextContainer = document.querySelector('.changed-text')
-let changedColorContainer = document.querySelector('.changed-color')
-let alternateTextContainer = document.querySelector('.alternate-text')
+const changedTextContainer = document.querySelector('.changed-text')
+const changedColorContainer = document.querySelector('.changed-color')
+const alternateTextContainer = document.querySelector('.alternate-text')
 let currentIndex = 0
 
 changedTextContainer.textContent = textArray[0]
@@ -15,3 +15,20 @@ setInterval(() => {
     currentIndex = (currentIndex + 1) % textArray.length
     changedTextContainer.textContent = textArray[currentIndex]
 }, 4000);
+
+
+// Audio player
+const audio = document.getElementById('audio-file')
+const controlButton = document.getElementById('control-audio-btn')
+const audioIcon = document.getElementById('audio-icon')
+const audioIconPath = 'assets/icons/'
+
+controlButton.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play()
+        audioIcon.src = audioIconPath + 'audio.svg'
+    } else {
+        audio.pause()
+        audioIcon.src = audioIconPath + 'mute-audio.svg'
+    }
+})
